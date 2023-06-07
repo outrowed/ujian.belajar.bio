3cae5a18bc7e062026600b5fa41b5ce240e99de4
outrowed <outrowed@gmail.com>
2023-06-07 13:44:46 +0700

    seperate date in data-loader into date-format
    Signed-off-by: outrowed <outrowed@gmail.com>

--- public/scripts/data-loader.js
+++ public/scripts/data-loader.js
@@ -1,5 +1,6 @@
 
 import { loadUserData } from "./user-data.js";
+import { formatDateToString, convertTimeToDate } from "./date-format.js";
 
 const user_data = loadUserData() ?? {
     name: "John Doe",
@@ -14,43 +15,12 @@ const user_data = loadUserData() ?? {
     opened_time: "7:00",
     closed_time: "8:30",
     submitted_time: "7:30",
+    time_limit: "01:30",
     is_null: true
 };
 
 if (user_data.is_null == null) {
     alert(`Please configure your profile at https://${window.location.hostname}/settings`);
-}
-
-function convertTimeToDate(time) {
-    if (!time.includes(":")) return new Date(time);
-
-    const date = new Date();
-    const [hour, minute] = time.split(":");
-
-    date.setHours(hour);
-    date.setMinutes(minute);
-
-    return date;
-}
-
-function formatDateToString(date) {
-    const localeDateString = date.toLocaleDateString("en-US", {
-        weekday: "long",
-        day: "numeric",
-        month: "long",
-        year: "numeric",
-        hour: "numeric",
-        minute: "2-digit"
-    });
-
-    let formattedDate = localeDateString.replace(/,/g, "").split(" ");
-    formattedDate = [
-        formattedDate[0], ",",
-        formattedDate[2], formattedDate[1], formattedDate[3], ",",
-        formattedDate[5], formattedDate[6]
-    ].join(" ").replace(/ ,/g, ",");
-
-    return formattedDate;
 }
 
 const convertToDateKeys = ["date", "opened_time", "closed_time", "submitted_time"];
@@ -59,6 +29,10 @@ const dataUKeyElements = document.querySelectorAll("[data-ukey]");
 for (const el of dataUKeyElements) {
     if (convertToDateKeys.includes(el.dataset.ukey)) {
         el.textContent = formatDateToString(convertTimeToDate(user_data[el.dataset.ukey]));
+    }
+    else if (el.dataset.ukey == "time_limit") {
+        const [hour, minute] = user_data.time_limit.split(":");
+        el.textContent = `${parseInt(hour)} hour ${minute} minutes`;
     }
     else {
         el.textContent = user_data[el.dataset.ukey];
