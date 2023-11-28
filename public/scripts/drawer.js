490ed7944b6c4e4aa4e63dca28584ce6c609d658
outrowed <outrowed@gmail.com>
2023-11-28 14:04:54 +0700

    revamp

--- public/scripts/drawer.js
+++
@@ -1,8 +0,0 @@
-
-const burger = document.querySelector("#burger");
-const drawer = document.querySelector("#drawer");
-const homeButton = document.querySelector("#home-button");
-
-burger.addEventListener("click", () => {
-    drawer.hidden = !drawer.hidden;
-});
--- public/scripts/url-data-loader.js
+++
@@ -1,6 +0,0 @@
-
-import { saveUserData } from "./user-data.js";
-
-const urlParams = new URLSearchParams(window.location.search);
-
-saveUserData(Object.fromEntries(urlParams.entries()));
